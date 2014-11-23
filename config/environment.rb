363eaf13ba432d2c9e047435d63882ae7bd3d851
laith <lalbarakat@gmail.com>
2014-11-23 15:18:38 -0400

    update environment for cas

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+#cas
+require 'casclient'
+require 'casclient/frameworks/rails/filter'
 # Initialize the rails application
 Uniqueidentifiers::Application.initialize!
 
