218e94cfa4d1205f448df0f5ba3be30ba2483dcf
laith <lalbarakat@gmail.com>
2014-11-23 16:53:11 -0400

    testing cas

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
 #before_filter RubyCAS::Filter
   def index
+
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,21 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 #cas
-#require 'casclient'
-#require 'casclient/frameworks/rails/filter'
 # Initialize the rails application
 Uniqueidentifiers::Application.initialize!
 
-#CASClient::Frameworks::Rails::Filter.configure(
-#      :cas_base_url => "https://cas.tamu.edu/"
-#    )
+# enable detailed CAS logging
+ cas_logger = CASClient::Logger.new(::Rails.root+'/log/cas.log')
+ cas_logger.level = Logger::DEBUG
+#
+ CASClient::Frameworks::Rails::Filter.configure(
+   :cas_base_url  => "https://cas.tamu.edu/",
+   :login_url     => "https://cas.tamu.edu/login",
+   :logout_url    => "https://cas.tamu.edu/logout",
+#   :validate_url  => "https://cas.example.foo/proxyValidate",
+#   :username_session_key => :cas_user,
+#   :extra_attributes_session_key => :cas_extra_attributes,
+   :logger => cas_logger,
+   :enable_single_sign_out => true
+   :service_url => "https://uniqueidentifiers.herokuapp.com/"
+)
