b3b4626477131979d0c8d2b734f8bf4cf2a764f4
laith <lalbarakat@gmail.com>
2014-11-23 17:05:57 -0400

    update

--- config/environment.rb
+++ config/environment.rb
@@ -5,17 +5,17 @@ require File.expand_path('../application', __FILE__)
 Uniqueidentifiers::Application.initialize!
 
 # enable detailed CAS logging
- cas_logger = CASClient::Logger.new(::Rails.root+'/log/cas.log')
- cas_logger.level = Logger::DEBUG
+# cas_logger = CASClient::Logger.new(::Rails.root+'/log/cas.log')
+# cas_logger.level = Logger::DEBUG
 #
- CASClient::Frameworks::Rails::Filter.configure(
-   :cas_base_url  => "https://cas.tamu.edu/",
-   :login_url     => "https://cas.tamu.edu/login",
-   :logout_url    => "https://cas.tamu.edu/logout",
+# CASClient::Frameworks::Rails::Filter.configure(
+#   :cas_base_url  => "https://cas.tamu.edu/",
+#   :login_url     => "https://cas.tamu.edu/login",
+#   :logout_url    => "https://cas.tamu.edu/logout",
 #   :validate_url  => "https://cas.example.foo/proxyValidate",
 #   :username_session_key => :cas_user,
 #   :extra_attributes_session_key => :cas_extra_attributes,
-   :logger => cas_logger,
-   :enable_single_sign_out => true,
-   :service_url => "https://uniqueidentifiers.herokuapp.com/",
-)
+#   :logger => cas_logger,
+#   :enable_single_sign_out => true,
+#   :service_url => "https://uniqueidentifiers.herokuapp.com/",
+#)
