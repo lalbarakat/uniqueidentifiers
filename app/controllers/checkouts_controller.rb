cb2d207da840bfe4c9df4a5eecce6d83bbf17dc4
Eric Nelson <ejn8411@tamu.edu>
2014-12-06 19:35:52 -0400

    removed extraneous comments

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -53,11 +53,7 @@ class CheckoutsController < AuthenticatedController
   
   # POST /review
   def review
-    #@student = Student.find_by_uin(params[:uin]) || 
-    #		Student.create(:firstname => params[:firstname], :lastname => params[:lastname], :email => params[:email], :phonenumber => params[:phonenumber], :uin => params[:uin])
-    #params[:checkout].delete :student
     @checkout = Checkout.new(params[:checkout])
-    #@checkout.student = @student
   end
 
   # POST /checkouts
