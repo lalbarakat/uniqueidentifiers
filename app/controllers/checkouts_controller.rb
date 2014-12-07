0eb99a68dd81bab7a63da0513a3a90eb458c69ec
Eric Nelson <ejn8411@tamu.edu>
2014-12-07 14:00:39 -0400

    Fix for firefox

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -61,6 +61,7 @@ class CheckoutsController < AuthenticatedController
   def create
     #@student = Student.find_by_uin(params[:uin]) || 
     #		Student.create(:firstname => params[:firstname], :lastname => params[:lastname], :email => params[:email], :phonenumber => params[:phonenumber], :uin => params[:uin])
+    
     @checkout = Checkout.new(params[:checkout])
     #@checkout.student = @student
 
