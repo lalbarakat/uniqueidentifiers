b9f9e08b2322d08173eb21cb6282616e5c3d8400
laith <lalbarakat@gmail.com>
2014-12-10 00:59:06 -0400

    add cas code

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+#require 'rack-cas/session_store/active_record'
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -62,6 +63,8 @@ module Uniqueidentifiers
 
 
 #    config.rubycas.cas_base_url = 'https://cas.tamu.edu/'
+    require 'rack-cas/session_store/active_record'
+    config.rack_cas.session_store = RackCAS::ActiveRecordStore
     config.rack_cas.server_url = 'https://cas.tamu.edu/'
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
+require 'rack-cas/session_store/rails/active_record'
+Uniqueidentifiers::Application.config.session_store :rack_cas_active_record_store
 Uniqueidentifiers::Application.config.session_store :cookie_store, key: '_uniqueidentifiers_session'
 
 # Use the database for sessions instead of the cookie-based default,
