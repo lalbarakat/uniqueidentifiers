64c7a8e0495cf6ed7292825d5233501e92accbce
Turmac <sysulqb@gmail.com>
2014-12-10 21:37:48 -0600

    the rspec tests
    the rspec tests for models

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+require 'faker'
+
+FactoryGirl.define do
+	factory :user do |f|
+		f.firstname { Faker::Name.first_name }
+		f.lastname { Faker::Name.last_name }
+		f.email { Faker::Internet.email }
+		f.password { Faker::Internet.password(8) }
+	end
+end
---
+++ spec/models/student_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Student do
+	it "has a valid factory" do
+		FactoryGirl.create(:student).should be_valid
+	end
+	
+	it "can insert student" do
+		FactoryGirl.build(:student, firstname: 'bruce', lastname: 'lee', email: 'aaa@google.com', uin: '12345678').should be_valid
+	end
+	
+	it "uin is 9-digest" do
+		FactoryGirl.build(:student, uin: '123456789').should be_valid
+	end
+	
+	it "uin should not be other than 9-digest" do
+		FactoryGirl.build(:student, uin: '12345678').should_not be_valid
+		FactoryGirl.build(:student, uin: '0123456789').should_not be_valid
+	end
+	
+end
