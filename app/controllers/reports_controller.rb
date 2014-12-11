92b97d3117a53aa0552e6cb2e0b99e1188e11ea5
Eric Nelson <ejn8411@tamu.edu>
2014-12-11 03:18:25 -0400

    More bugfixes for report generation

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,12 +8,12 @@ class ReportsController < ApplicationController
   
   def by_checkout_date
   	checkout_date = Date.parse(params[:year] + params[:month] + params[:day])
-  	@checkouts = Checkout.joins(:checkedout_items).where('checkedout_items.startdate BETWEEN ? AND ?', checkout_date.beginning_of_day, checkout_date.end_of_day).group(:id)
+  	@checkouts = Checkout.joins(:checkedout_items).where('checkedout_items.startdate BETWEEN ? AND ?', checkout_date.beginning_of_day, checkout_date.end_of_day).group('checkouts.id')
   end
   
   def by_return_date
   	return_date = Date.parse(params[:year] + params[:month] + params[:day])
-  	@checkouts = Checkout.joins(:checkedout_items).where('checkedout_items.enddate BETWEEN ? AND ?', return_date.beginning_of_day, return_date.end_of_day).group(:id)
+  	@checkouts = Checkout.joins(:checkedout_items).where('checkedout_items.enddate BETWEEN ? AND ?', return_date.beginning_of_day, return_date.end_of_day).group('checkouts.id')
   end
   
   def by_student
