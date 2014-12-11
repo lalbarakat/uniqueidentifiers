4eca9de1aaf180c588420bea45ab8c08403679fc
laith <lalbarakat@gmail.com>
2014-12-11 03:47:01 -0400

    add report by student

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -16,8 +16,13 @@ class ReportsController < ApplicationController
   	@checkouts = Checkout.joins(:checkedout_items).where('checkedout_items.enddate BETWEEN ? AND ?', return_date.beginning_of_day, return_date.end_of_day).all
   end
   
+  
   def by_student
-  	@checkouts = Checkout.joins(:student).where('students.uin = ?', params[:uin]).all
+	sql ="SELECT STUDENTS.*, CHECKOUTS.*,CHECKEDOUT_ITEMS.*,ITEMS.*  FROM STUDENTS, CHECKOUTS,CHECKEDOUT_ITEMS,ITEMS WHERE STUDENTS.ID=CHECKOUTS.STUDENT_ID AND CHECKOUTS.ID=CHECKOUT_ID AND CHECKEDOUT_ITEMS.ITEM_ID=ITEMS.ID AND STUDENTS.UIN='".params[:studentid]."'"
+	@result = ActiveRecord::Base.connection.execute(sql)
+
+  	#@checkouts = Checkout.joins(:student).where('students.uin = ?', params[:uin]).all
+	#@checkouts = Checkout.joins(:student).where(students => {uin =>123456789})
   	#@students = @checkouts.collect { |checkout| checkout.student }
   	#@items = @checkouts.collect { |checkout| checkout.checked_ }
   end
