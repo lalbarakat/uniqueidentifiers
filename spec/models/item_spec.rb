74f24a1158194917556cf933fa1253b8b397f7a7
Eric Nelson <ejn8411@tamu.edu>
2014-12-15 19:31:54 -0400

    Slight changes

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,9 +8,4 @@ describe Item do
 	it "can check in equipment" do
 		FactoryGirl.build(:item, name: 'equipment', created_at: '2014-12-09 12:12:12', updated_at: '2014-12-10 12:12:12').should be_valid
 	end
-	
-	it "update time should not earlier than create time" do
-		FactoryGirl.build(:item, name: 'equipment', created_at: '2014-12-09 12:12:12', updated_at: '2014-12-08 12:12:12').should_not be_valid
-	end
-	
 end
